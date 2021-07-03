f6df63d7f05fdd849439db2ae6adea2f19d51349
Daniel Infante <danielpinto105@gmail.com>
2021-07-03 21:59:44 +0100

    finish with the Todo Appgit add -A :D

--- javaScript/todoProject/app.js
+++ javaScript/todoProject/app.js
@@ -2,12 +2,11 @@ const list = [];
 
 let wish = prompt('What would you like to do?');
 
-
 while (wish !== 'quit' && wish !== 'q'){
     if (wish === 'list') {
         console.log('**************************');
         for (i = 0; i < list.length; i++){
-            console.log(`${i + 1}: ${list[i]}`)
+            console.log(`${i}: ${list[i]}`)
         }
         console.log('**************************')
     } else if (wish === 'new'){
@@ -15,16 +14,16 @@ while (wish !== 'quit' && wish !== 'q'){
         list.push(newItem);
         
         console.log(`${newItem} added to the list`)
-    
-    } 
+    } else if (wish === 'delete'){
+        let itemToDel = prompt('enter the number you want to delete')
+        let delItem = list.splice(itemToDel, 1);
+        console.log(`You have deleted ${delItem}`)
+    }
     wish = prompt('What would you like to do?');
-   
 } 
-
-
-
-
 console.log('YOU HAVE QUIT THE APP')
+
+
 
 // for (i = 0; wish === 'new'; i++) {
 //     let newItem = list.push(prompt('Enter new todo'))
