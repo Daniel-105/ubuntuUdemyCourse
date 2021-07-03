99b450c49ae423b0f0dd9a448603c9fc03da312c
Daniel Infante <danielpinto105@gmail.com>
2021-07-03 13:09:18 +0100

    getting somewhere

--- javaScript/todoProject/app.js
+++ javaScript/todoProject/app.js
@@ -4,22 +4,18 @@ let wish = prompt('What would you like to do?');
 
 let i = 0
 while (wish !== 'quit'){
-    wish = prompt('What would you like to do?');
+    
 i++
     if (wish === 'new'){
         let newItem = list.push(prompt('Enter new todo'))
-        let wish = prompt('What would you like to do?');
     } else if (wish === 'list') {
-        console.log(list)
-        let wish = prompt('What would you like to do?');
+        console.log(`${list[i]}`)
     }
-    
+    wish = prompt('What would you like to do?');
     
 } 
 
-if (wish === 'list'){
-    console.log(list.length[i])
-}
+
 
 
 console.log('YOU HAVE QUIT THE APP')
