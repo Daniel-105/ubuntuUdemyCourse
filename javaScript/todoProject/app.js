05b4e3e29e3c2c594e099402513062bc117020e5
Daniel Infante <danielpinto105@gmail.com>
2021-07-03 10:56:09 +0100

    Still trying stuff to build the todo app

--- javaScript/todoProject/app.js
+++ javaScript/todoProject/app.js
@@ -2,24 +2,49 @@ let list = [];
 
 let wish = prompt('What would you like to do?');
 
-while (wish !== 'quit'){
-
 let i = 0
-while (wish === 'new'){
-
-    let newItem = list.push(prompt('Enter new todo'))
-    let wish = prompt('What would you like to do?');
-    // if (wish === 'list') {
-    //     console.log(list)
-    //     let wish = prompt('What would you like to do?');
-    // }
-    i++
-    if (wish !== 'new') break
+while (wish !== 'quit'){
+    wish = prompt('What would you like to do?');
+i++
+    if (wish === 'new'){
+        let newItem = list.push(prompt('Enter new todo'))
+        let wish = prompt('What would you like to do?');
+    } else if (wish === 'list') {
+        console.log(list)
+        let wish = prompt('What would you like to do?');
+    }
+    
+    
 } 
 
 if (wish === 'list'){
     console.log(list.length[i])
 }
-}
+
 
 console.log('YOU HAVE QUIT THE APP')
+
+// for (i = 0; wish === 'new'; i++) {
+//     let newItem = list.push(prompt('Enter new todo'))
+//     let wish = prompt('What would you like to do?');
+//     if (wish !== 'new') break
+// }
+// console.log('*******************************')
+// if (wish === 'list') {
+//     console.log(list[i])
+// }
+
+
+// let i = 0;
+// if (wish ==='new') {
+//     while (wish) {
+//         i++
+//     let newItem = list.push(prompt('Enter new todo'))
+//     let wish = prompt('What would you like to do?');
+//     }
+// } else if (wish === 'list') {
+//     console.log(list[i])
+// }
+
+
+
