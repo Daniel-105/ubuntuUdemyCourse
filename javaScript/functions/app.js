611094edca3814e8d78b53435c678eb6a02b256f
Daniel Infante <danielpinto105@gmail.com>
2021-07-12 17:51:27 +0100

    first function

---
+++ javaScript/functions/app.js
@@ -0,0 +1,15 @@
+// function greet(firstName) {
+//     console.log(`First Name is: ${firstName}`)
+// }
+// greet("António")
+
+function nums() {
+    for (i = 0; i < 10; i++) {
+        for (j = 0; j < 10; j++) {
+            console.log(i, j)
+        }
+    }
+    console.log(1,0,0)
+}
+
+nums()
