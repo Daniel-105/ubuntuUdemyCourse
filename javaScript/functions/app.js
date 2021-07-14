e605b95d64bcdbb7a5f779c34d52a1a941a953fc
Daniel Infante <danielpinto105@gmail.com>
2021-07-14 14:15:03 +0100

    returned learned. Trying to figure out lastElement exercise

--- javaScript/functions/app.js
+++ javaScript/functions/app.js
@@ -14,7 +14,43 @@
 
 // nums()
 
-function repeat(message, times) {
-    console.log(message * times.parseInt())
+// function repeat(message, times) {
+//     let result = '';
+//     for(let i = 0; i < times; i++) {
+//      result += message;
+       
+//     }
+//     console.log(result) 
+// }
+// repeat("hello", 4)
+
+// function isSnakeEyes(dice1, dice2){
+//     if (dice1 === 1 && dice2 === 1) {
+//     console.log("Snake Eyes!")
+// } else {
+//     console.log("Not Snake Eyes!")
+// }
+// }
+// isSnakeEyes(6,6)
+
+
+// Attempt 1
+// function lastElement([x,y,z]) {
+//     if (z) {
+//         return z
+//     } else if (y) {
+//         return y
+//     } else if (x) {
+//         return x
+//     }
+//     else {
+//         return null
+//     }
+// }
+
+// Attemp 2
+
+function lastElement(value) {
+    let result = [];
+    
 }
-repeat("hello", 4)
