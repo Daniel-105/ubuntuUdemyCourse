8e4d360ec32cf1084b0fc0e230e10cd0dcd5f3cc
Daniel Infante <danielpinto105@gmail.com>
2021-09-01 15:44:25 +0100

    Learning params

---
+++ javaScript/defaultParams/app.js
@@ -0,0 +1,24 @@
+// const rollDie = (numSides) => {
+//   if (!numSides) {
+//     numSides = 6;
+//   }
+//   return Math.floor(Math.random() * numSides) + 1;
+// };
+
+// const rollDie = (numSides = 6) => {
+//   return Math.floor(Math.random() * numSides) + 1;
+// };
+
+// function greet(person = "Zé", msg = "Óhh", punc = "!!!!") {
+//   console.log(`${msg}, ${person}${punc}`);
+// }
+
+// const cats = ["Blue", "Scout", "Rocket"];
+// const dogs = ["Rusty", "Wyatt"];
+
+// dogs.push("Neskuiq");
+
+// const allPets = [...cats, ...dogs];
+
+const greet = ["Hello"];
+const leters = [...greet[0]];
