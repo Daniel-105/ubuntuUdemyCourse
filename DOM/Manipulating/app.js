82eb14c8f96738c919c6b5c9e4033113fbad0cf9
Daniel Infante <danielpinto105@gmail.com>
2021-09-13 18:57:49 +0100

    Figuring out the exercise

--- DOM/Manipulating/app.js
+++ DOM/Manipulating/app.js
@@ -1,12 +1,29 @@
-const allLinks = document.querySelectorAll('a');
+// const allLinks = document.querySelectorAll("a");
 
 // for (let link of allLinks) {
 //     link.innerText = 'I AM A LINK!!!!'
 // }
 
+// for (let link of allLinks) {
+//     link.style.color = 'rgb(0, 108, 134)';
+//     link.style.textDecorationColor = 'magenta';
+//     link.style.textDecorationStyle = 'wavy'
+// }
 
-for (let link of allLinks) {
-    link.style.color = 'rgb(0, 108, 134)';
-    link.style.textDecorationColor = 'magenta';
-    link.style.textDecorationStyle = 'wavy'
+// for (link of allLinks) {
+//   link.style.color = "red";
+// }
+
+const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+const letters = document.querySelectorAll("span");
+for (letter of letters) {
+  for (i = 0; i < colors.length; i++) {
+    letter.style.color = "colors[i]";
+  }
 }
+// for (let i = 0; i < letters.length; i++) {
+//   for (let i = 0; i < colors.length; i++) {
+//     letters.style.color = "colors[i]";
+//   }
+// }
