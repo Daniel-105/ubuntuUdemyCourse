83ad7479ea627c32020fe6d879ca774417cc9452
Daniel Infante <danielpinto105@gmail.com>
2021-09-14 10:17:33 +0100

    Found a solution for the problem

--- DOM/Manipulating/app.js
+++ DOM/Manipulating/app.js
@@ -17,15 +17,26 @@
 const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 
 const letters = document.querySelectorAll("span");
-// for (let letter of letters) {
-//   for (i = 0; i < colors.length; i++) {
-//     letter.style.color = "colors[i]";
-//   }
-// }
+
+// letters[0].style.color = colors[0];
+// letters[1].style.color = colors[1];
+// letters[2].style.color = colors[2];
+// letters[3].style.color = colors[3];
+// letters[4].style.color = colors[4];
+// letters[5].style.color = colors[5];
+// letters[6].style.color = colors[6];
+
+let lettersArray = Array.prototype.slice.call(letters);
+
+for (let letter of lettersArray) {
+  for (i = 0; i < lettersArray.length; i++) {
+    letter.style.color = "colors[i]";
+  }
+}
 // for (let i = 0; i < letters.length; i++) {
 //   for (let i = 0; i < colors.length; i++) {
 //     letters.style.color = "colors[i]";
 //   }
 // }
 
-window.getComputedStyle("span") = 'red';
+// window.getComputedStyle("span") = 'red';
