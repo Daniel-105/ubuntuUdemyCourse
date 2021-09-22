c16b62c0b6779ce8a83822abfc54f64a3c888f01
Daniel Infante <danielpinto105@gmail.com>
2021-09-22 16:19:01 +0100

    Grocery list app done. Trying to find why it doesn't work on udemy platform

--- DOM/projects/tweet-project/app.js
+++ DOM/projects/tweet-project/app.js
@@ -1,20 +1,42 @@
-const tweetForm = document.querySelector("form");
-const usernameInput = tweetForm.elements.username;
-const tweetInput = tweetForm.elements.tweet;
-const tweets = document.querySelector("#tweets");
+// const tweetForm = document.querySelector("form");
+// const usernameInput = tweetForm.elements.username;
+// const tweetInput = tweetForm.elements.tweet;
+// const tweets = document.querySelector("#tweets");
 
-tweetForm.addEventListener("submit", function (e) {
+// tweetForm.addEventListener("submit", function (e) {
+//   e.preventDefault();
+//   appendElements(usernameInput.value, tweetInput.value);
+//   usernameInput.value = "";
+//   tweetInput.value = "";
+// });
+
+// function appendElements(userName, tweet) {
+//   const newTweet = document.createElement("li");
+//   const bTag = document.createElement("b");
+//   bTag.append(usernameInput.value);
+//   newTweet.append(bTag);
+//   newTweet.append(` - ${tweetInput.value}`);
+//   tweets.append(newTweet);
+// }
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
   e.preventDefault();
-  appendElements(usernameInput.value, tweetInput.value);
-  usernameInput.value = "";
-  tweetInput.value = "";
+
+  const productInput = form.elements.product;
+  const qtyInput = form.elements.qty;
+  const list = document.querySelector("#list");
+
+  appendItem(qtyInput.value, productInput.value);
+
+  productInput.value = "";
+  qtyInput.value = "";
 });
 
-function appendElements(userName, tweet) {
-  const newTweet = document.createElement("li");
-  const bTag = document.createElement("b");
-  bTag.append(usernameInput.value);
-  newTweet.append(bTag);
-  newTweet.append(` - ${tweetInput.value}`);
-  tweets.append(newTweet);
+function appendItem(qty, product) {
+  const listItem = document.createElement("li");
+  listItem.append(qty);
+  listItem.append(` ${product}`);
+  list.append(listItem);
 }
